09c27268441a11622dc7094d36616af97fb3534c
Duong TRAN <trandinhnamduong@gmail.com>
2021-06-24 13:58:24 +0700

    modify server.js adding additonal function of chat

--- client/client.js
+++ client/client.js
@@ -1,6 +1,10 @@
 const io = require("socket.io-client");
 const socket = io("http://localhost:3000");
 var nickname = null;
+var s_pattern = /^s;([A-Z0-9]+);(.+)/i;
+
+const usersSocket = io("http://localhost:3000/users");
+const ordersSocket = io("http://localhost:3000/orders");
 
 console.log("Connecting to the server...");
 
@@ -30,6 +34,48 @@ rl.on("line", (input) => {
     else if ("q;" === input) {
         socket.emit("quit", {"sender": nickname, "action": "quit"});
     }
+    else if ("tr;" === input) { // Tracing : tr
+        socket.emit("trace");
+    }
+    else if (true === s_pattern.test(input)) {
+        var info = input.match(s_pattern);
+        socket.emit("send", {"sender": nickname, "action": "send", "receiver": info[1], "msg": info[2]});
+    }
+    // join group function
+    else if (true === input.startsWith("jg;")) {
+        var str = input.slice(3);
+        socket.emit("join_group", {"sender": nickname, "action": "join_group", "group": str});
+    }
+    // broadcast function
+    else if (true === bg_pattern.test(input)) {
+        var info = input.match(bg_pattern);
+        socket.emit("broadcast_group", {"sender": nickname, "action": "broadcast_group", "group": info[1], "msg": info[2]});
+    }
+    // list member of a group
+    else if (true === input.startsWith("mbr;")) {
+        var str = input.slice(4);
+        socket.emit("list_members_group", {"sender": nickname, "action": "list_members_group", "group": str});
+    }
+    // List message group
+    else if (true === input.startsWith("msg;")) {
+        var str = input.slice(4);
+        socket.emit("list_messages_group", {"sender": nickname, "action": "list_messages_group", "group": str});
+    }
+    //list group function
+    else if ("grp;" === input) {
+        socket.emit("list_groups", {"sender": nickname, "action": "list_groups"});
+    }
+    // leave group function
+
+    else if (true === input.startsWith("lg;")) {
+        var str = input.slice(3);
+        socket.emit("leave_group", {"sender": nickname, "action": "leave_group", "group": str});
+    }
+    // list message user (self implement)
+    // else if (true === input.startsWith(";")) {
+    //     var str = input.slice(4);
+    //     socket.emit("list_messages_user", {"sender": nickname, "action": "list_messages_user", "user": str});
+    // }
 });
 
 socket.on("broadcast", (data) => {
@@ -51,4 +97,46 @@ socket.on("quit", (data) => {
     console.log("[INFO]: %s quit the chat", data.sender);
 });
 
+// Display message showing a person joined group....
+
+socket.on("join_group", (data) => {
+    console.log("[INFO]: %s has joined the group", data.sender);
+});
+
+// display message on group
+socket.on("broadcast_group", (data) => {
+    console.log("%s", data.msg);
+});
+// list member in group
+socket.on("list_members_group", (data) => {
+    console.log("[INFO]: List of members:");
+    for (var i = data.members.length - 1; i >= 0; i--) {
+        console.log(data.members[i]);
+    }
+});
+// List message group
+socket.on("list_messages_group", (data) => {
+    console.log("[INFO]: History of messages:");
+    for (var i = data.msgs.length - 1; i >= 0; i--) {
+        console.log(data.msgs[i]);
+    }
+});
+// List group
+socket.on("list_groups", (data) => {
+    console.log("[INFO]: List of groups:");
+    for (var i = data.groups.length - 1; i >= 0; i--) {
+        console.log(data.groups[i]);
+    }
+});
+// display a client left group
+socket.on("leave_group", (data) => {
+    console.log("[INFO]: %s left the group", data.sender);
+});
+// list message user
+// socket.on("list_messages_user", (data) => {
+//     console.log("[INFO]: History of user messages:");
+//     for (var i = data.msgs.length - 1; i >= 0; i--) {
+//         console.log(data.msgs[i]);
+//     }
+// });
 
--- server/server.js
+++ server/server.js
@@ -31,9 +31,95 @@ io.on("connect", (socket) => {
     });
 
     socket.on("quit", (data) => {
-    console.log("\n%s", data);
-    socket.broadcast.emit("quit", data);
-    socket.disconnect(true);
+        console.log("\n%s", data);
+        socket.broadcast.emit("quit", data);
+        socket.disconnect(true);
     });
+    // Listen to the 'trace' event emitted from the client on 12/06/2021
+    socket.on("trace", () => {
+        console.log("\n=============== Trace ===============");
+        console.log(io.of("/"));
+    });
+});
+// using namespace
+
+const usersNsp = io.of("/users");
+// usersNsp.on("connect", (socket) => {
+
+// using default namespace for sake of simplicity
+io.of("/").on("connect", (socket) => {
+    console.log("Listening users connected in namespace");
+    socket.on("send", (data) => {
+        console.log("\n%s", data);
+        
+        var socket_id = null;
+        for (const [key, value] of io.of("/").sockets) {
+            if (data.receiver.toLowerCase() === value.nickname) {
+                socket_id = key;
+            }
+        }        if (socket_id !== null) {
+            io.of("/").to(socket_id).emit("send", data);
+        }
+    });
+    socket.on("join_group", (data) => {
+        console.log("\n%s", data);
+        socket.join(data.group);
+        console.log("Group: ", data.group, ", Joined: ", data.sender);
+        io.of("/").to(data.group).emit("join_group", data);
+    });
+    socket.on("broadcast_group", (data) => {
+        console.log("\n%s", data);
+        socket.to(data.group).emit("broadcast_group", data);
+        if (undefined === io.of("/").room_messages) {
+            io.of("/").room_messages = {};
+        }        if (undefined === io.of("/").room_messages[data.group]) {
+            io.of("/").room_messages[data.group] = [];
+        }        io.of("/").room_messages[data.group].push(data.msg);
+    });
+    socket.on("list_members_group", (data) => {
+        console.log("\n%s", data);        
+        var socket_ids;
+        var members = [];
+        for (const [key, value] of io.of("/").adapter.rooms) {
+            if (key === data.group) {
+                socket_ids = value;
+            }
+        }        socket_ids.forEach((socket_id) => {
+            const socket_in_room = io.of("/").sockets.get(socket_id);
+            members.push(socket_in_room.nickname);
+        });        socket.emit("list_members_group", {"sender": data.sender, "action": "list_members_group", "group": data.group, "members": members});
+    });
+    socket.on("list_messages_group", (data) => {
+        console.log("\n%s", data);        
+        var msgs = io.of("/").room_messages[data.group];
+        socket.emit("list_messages_group", {"sender": data.sender, "action": "list_messages_group", "group": data.group, "msgs": msgs});
+    });
+    // to list group
+    socket.on("list_groups", (data) => {
+        console.log("\n%s", data);        var groups = [];        for (const [key, value] of io.of("/").adapter.rooms) {
+            if (false === value.has(key)) {
+                groups.push(key);
+            }
+        }        socket.emit("list_groups", {"sender": data.sender, "action": "list_groups", "groups": groups});
+    });
+    // leave group function
+    socket.on("leave_group", (data) => {
+        console.log("\n%s", data);
+        
+        socket.leave(data.group);
+        console.log("Group: ", data.group, ", Left: ", data.sender);
+        io.of("/").to(data.group).emit("leave_group", data);
+    });
+    // list message users
+    socket.on("list_messages_user", (data) => {
+        console.log("\n%s", data);        
+        var msgs = io.of("/").room_messages[data.user];
+        socket.emit("list_messages_user", {"sender": data.sender, "action": "list_messages_users", "user": data.user, "msgs": msgs});
+    });
+});
+
+const ordersNsp = io.of("/orders");
+ordersNsp.on("connect", (socket) => {
+    //...
 });
 
